e24981bd44741ca41c0a848aaf69e84a02d3265c
ewdager <ewdager@hotmail.com>
2017-08-14 18:40:15 +0800

    fix wechat api

--- robot.py
+++ robot.py
@@ -1,7 +1,20 @@
+# -*- coding: utf-8 -*-
 from werobot import WeRoBot
 
 robot = WeRoBot(token='test01')
 
+robot.config["APP_ID"] = "wx898a5c4d88250e4c"
+robot.config["APP_SECRET"] = "d4624c36b6795d1d99dcf0547af5443d"
+
+client = robot.client
+
+client.create_menu({
+    "button":[{
+         "type": "view",
+         "name": u"加入科协",
+          "url":"http://119.29.25.247/index/"
+    }]
+})
 
 @robot.handler
 def hello(message):
