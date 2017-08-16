705ee6aec8cc245e772cbb825ebd82ada5a6d231
ewdager <ewdager@hotmail.com>
2017-08-16 22:08:00 +0800

    fix form

--- SignUp/forms.py
+++ SignUp/forms.py
@@ -9,18 +9,25 @@ class ContactForm(forms.Form):
         [u'物联网工程', u'物联网工程'],
         [u'软件工程', u'软件工程']
     ]
-    WISH_CHOICE = [
-        [u'网络部', u'科协网络部'],
-        [u'创业部', u'科协创业部'],
-        [u'竞赛部', u'科协竞赛部'],
-        [u'社团部', u'科协社团部']
+    WISH_CHOICE1 = [
+        [u'网络部', u'第一志愿：科协网络部'],
+        [u'创业部', u'第一志愿：科协创业部'],
+        [u'竞赛部', u'第一志愿：科协竞赛部'],
+        [u'社团部', u'第一志愿：科协社团部']
+    ]
+    WISH_CHOICE2 = [
+        [u'网络部', u'第二志愿：科协网络部'],
+        [u'创业部', u'第二志愿：科协创业部'],
+        [u'竞赛部', u'第二志愿：科协竞赛部'],
+        [u'社团部', u'第二志愿：科协社团部'],
+        [u'无', u'我只想进上面那个部门（慎重考虑）'],
     ]
     name = forms.CharField(max_length=200, label=u'姓名', widget=forms.TextInput(attrs={'placeholder': u'姓名'}))
     major = forms.ChoiceField(choices=MAJOR_CHOICE, label=u'专业')
     phone_num = forms.CharField(max_length=200, label=u'电话', widget=forms.TextInput(attrs={'placeholder': u'电话号码'}))
     email = forms.EmailField(label=u'邮箱', widget=forms.EmailInput(attrs={'placeholder': u'常用邮箱地址'}))
     qq = forms.CharField(max_length=200, label=u'QQ', widget=forms.TextInput(attrs={'placeholder': u'QQ'}))
-    first_choice = forms.ChoiceField(choices=WISH_CHOICE, label=u'第一志愿')
-    second_choice = forms.ChoiceField(choices=WISH_CHOICE, label=u'第二志愿')
+    first_choice = forms.ChoiceField(choices=WISH_CHOICE1, label=u'第一志愿')
+    second_choice = forms.ChoiceField(choices=WISH_CHOICE2, label=u'第二志愿')
     recognize = forms.CharField(label=u'对科协的认识', widget=forms.Textarea(attrs={'placeholder': u'对科协的认识', 'class': 'desciption'}))
     introduction = forms.CharField(label=u'自我介绍', widget=forms.Textarea(attrs={'placeholder': u'自我介绍', 'class': 'desciption'}))
